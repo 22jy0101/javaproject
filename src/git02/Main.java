f128ddafc0c797d823d8368d40529f45aaaf8ceb
22jy0101 <22jy0101@PC23.jynet2.jec.ac.jp>
2023-10-04 10:27:31 +0900

    情報システム開発科と表示

--- src/git02/Main.java
+++ src/git02/Main.java
@@ -3,7 +3,8 @@ package git02;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("日本電子専門学校");
+		System.out.println("日本電子専門学校");
+		System.out.println("情報システム開発科");
 	}
 
 }
